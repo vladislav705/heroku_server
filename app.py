6abe3187ff967d6e8256593c33779b09a899b8c8
vladislav705 <92874630+vladislav705@users.noreply.github.com>
2022-11-20 00:52:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import pandas as pd
-import os
-from binance.client import Client
-import pandas as pd
-import datetime, time
-import numpy as np
-from datetime import datetime, timedelta
+# import pandas as pd
+# import os
+# from binance.client import Client
+# import pandas as pd
+# import datetime, time
+# import numpy as np
+# from datetime import datetime, timedelta
 from flask import Flask
 from flask import request, render_template
 from functions import GetHistoricalData, make_new_col, bootstrap, dataset_for_current_pred
@@ -20,13 +20,13 @@ def serve():
 @app.route('/interval', methods = ['GET'])
 def interval():
 
-    df_current = dataset_for_current_pred(Client)
-    # Добавляем новую колонку
-    df_current = make_new_col(df_current)
-    # требуется для абсолютных значений запомнить текущую
-    current_value = df_current['open'].values.astype(float)[-1]
-    res_interval = bootstrap(df_current['change'].values, n_forcast=24, intervals_in_hour=60)*current_value
-    
+#     df_current = dataset_for_current_pred(Client)
+#     # Добавляем новую колонку
+#     df_current = make_new_col(df_current)
+#     # требуется для абсолютных значений запомнить текущую
+#     current_value = df_current['open'].values.astype(float)[-1]
+#     res_interval = bootstrap(df_current['change'].values, n_forcast=24, intervals_in_hour=60)*current_value
+    res_interval = [0.1, 0.2]
     return render_template('index.html', prediction_text='Interval: {} {}'.format(res_interval[0], res_interval[1]))
 
 
